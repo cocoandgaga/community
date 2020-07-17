de5d3c8c40c01d02eb652b52e05fdb28bc87fb60
cocoandgaga <1004027030@qq.com>
2020-07-17 16:29:43 +0800

    add:markdown

--- src/main/java/com/zxj/community/controller/FileTransportController.java
+++ src/main/java/com/zxj/community/controller/FileTransportController.java
@@ -1,21 +1,21 @@
 package com.zxj.community.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.zxj.community.dto.FileDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class FileTransportController {
-    @RequestMapping("/file/upload")
+    @RequestMapping(value = "/file/upload",method = RequestMethod.POST)
     @ResponseBody
-    public String upload() {
+    public FileDTO upload() {
         FileDTO fileDTO=new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/img/wechat.png");
-        String json=JSON.toJSONString(fileDTO);
-        return json;
+        return fileDTO;
     }
 
 
